6a293f34c68c4a9787ab68e1e215ec1fcdd188d2
PreethiECE <preethiece96@gmail.com>
2016-08-22 13:21:10 +0530

    Update and rename power1.java to reverse.java

---
+++ reverse.java
@@ -0,0 +1,9 @@
+public static void main(String[] arg)
+{
+Scanner in=new Scanner(System.in);
+String str;
+System.out.println("Enter string");
+str=in.nextLine;
+str=new StringBuffer(str).reverse().toString();
+System.out.println(str);
+}
