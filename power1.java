d88ff89a8b457c6e67516e5878fb42683256c64c
PreethiECE <preethiece96@gmail.com>
2016-08-22 12:14:40 +0530

    Rename power.java to power1.java

---
+++ power1.java
@@ -0,0 +1,8 @@
+public static void main(String[] arg)
+{
+Scanner in=new Scanner(System.in);
+int a;
+System.out.println("Enter number");
+a=in.nextInt();
+System.out.println((int)Math.pow(a,a));
+}
